359d541bb37bb519960accefddcdb9b06b15393f
Luis Gabriel <lgcastellanos76@misena.edu.co>
2023-04-30 19:02:04 -0500

    menu animations

--- js/app.js
+++ js/app.js
@@ -1,10 +1,41 @@
-const App = {
-	data() {
-		return {};
+$(document).on('ready', function () {
+
+	var offsets = [];
+	var items = $('.menu-item');
+
+	const setOffsets = () => {
+		for (i = 0; i < items?.length; i++) {
+			let el = $(items[i]);
+			let pos = el?.offset()?.top;
+			if (pos)
+				offsets.push(pos);
+		}
 	}
-}
 
-const app = Vue.createApp(App);
+	const itemClick = (e) => {
+		$('.panel-body ul li').removeClass('active');
+		e?.target?.parentNode?.setAttribute('class','active');
+	};
 
-app.component('example', {
+	const scrollItem = (e) => {
+		var position = $(e.target).offset().top;
+
+	    if ($(window).scrollTop() == position) {
+	    	$('.panel-body ul li').removeClass('active');
+			e?.target?.parentNode?.setAttribute('class','active');
+	    }
+	};
+
+	$('.left-panel ul li a').on('click', itemClick);
+
+	$(window).scroll(() => {
+		for (i = 0; i < offsets?.length; i++) {
+			if ($(window).scrollTop() >= (offsets[i] - 30)) {
+				$('.panel-body ul li').removeClass('active');
+				$(`.panel-body ul li a[href=#${items[i]?.id}]`).parent().addClass('active');
+			}
+		}
+	});
+
+	setOffsets();
 });
